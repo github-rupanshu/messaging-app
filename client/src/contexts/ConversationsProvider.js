b85b810a9d860ef5a21f663016f0d865d650e0df
rupanshu647 <rupanshusharama7@gmail.com>
2021-06-12 11:27:45 +0530

    added functionality to send message in conversation

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -21,43 +21,52 @@ export function ConversationsProvider({ id, children }) {
     });
   }
   function addMessageToConversation({ recipients, text, sender }) {
-    setConversations(prevConversations=>{
-      let madeChange=false;
-      const newMessage={sender,text}
-      const newConversations=prevConversations.map(conversation=>{
-        if(arrayEquality(conversation.recipients,recipients)){
-          madeChange=true;
-          return{
+    setConversations((prevConversations) => {
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConversations.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
             ...conversation,
-            messages:[...conversation.messages,newMessage]
-          }
+            messages: [...conversation.messages, newMessage],
+          };
         }
         return conversation;
-      })
-      if(madeChange){
+      });
+      if (madeChange) {
         return newConversations;
-      }else{
-        return[
-          ...prevConversations,{recipients,messages:[newMessage]}
-        ]
+      } else {
+        return [...prevConversations, { recipients, messages: [newMessage] }];
       }
-    })
+    });
   }
 
   function sendMessage(recipients, text) {
     addMessageToConversation({ recipients, text, sender: id });
   }
   const formattedConversations = conversations.map((conversation, index) => {
-    const recipients = conversation.recipients.map((recipient) => {
-      const contact = contacts.find((contact) => {
-        return contact.id === recipient;
-      });
-      const name = (contact && contact.name) || recipient;
-      return { id: recipient, name };
-    });
-    const selected = index === selectedConversationIndex;
-    return { ...conversations, recipients, selected };
-  });
+    const recipients = conversation.recipients.map(recipient => {
+      const contact = contacts.find(contact => {
+        return contact.id === recipient
+      })
+      const name = (contact && contact.name) || recipient
+      return { id: recipient, name }
+    })
+
+    const messages = conversation.messages.map(message => {
+      const contact = contacts.find(contact => {
+        return contact.id === message.sender
+      })
+      const name = (contact && contact.name) || message.sender
+      const fromMe = id === message.sender
+      return { ...message, senderName: name, fromMe }
+    })
+    
+    const selected = index === selectedConversationIndex
+    return { ...conversation, messages, recipients, selected }
+  })
+  
   const value = {
     conversations: formattedConversations,
     selectedConversation: formattedConversations[selectedConversationIndex],
@@ -72,12 +81,11 @@ export function ConversationsProvider({ id, children }) {
   );
 }
 
-
-function arrayEquality(a,b){
-  if(a.length !== b.length) return false;
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
   a.sort();
   b.sort();
-  return a.every((element,index)=>{
-    return element===b[index]
-  })
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }

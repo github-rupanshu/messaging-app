718feea6000cff5741a0188966aac2dd16d679db
Rupanshu <mail2rupanshu@gmail.com>
2021-06-12 08:53:59 +0530

    worked on sending messages and adding in conversation in local storage

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,40 +1,83 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
-import  {useContacts} from  './ContactsProvider'
+import { useContacts } from "./ContactsProvider";
 const ConversationsContext = React.createContext();
 
-export function useConversations(){
-    return useContext(ConversationsContext);
+export function useConversations() {
+  return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
-  const [conversations, setConversations] = useLocalStorage("conversations", []);
-  const [selectedConversationIndex,setSelectedConversationIndex]=useState(0)
-  const {contacts}=useContacts();
+export function ConversationsProvider({ id, children }) {
+  const [conversations, setConversations] = useLocalStorage(
+    "conversations",
+    []
+  );
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+  const { contacts } = useContacts();
+
   function createConversations(recipients) {
     setConversations((prevConversations) => {
-      return [...prevConversations, {recipients,messages:[] }];
+      return [...prevConversations, { recipients, messages: [] }];
     });
   }
-  const formattedConversations=conversations.map((conversation,index)=>{
-    const recipients=conversation.recipients.map(recipient=>{
-      const contact=contacts.find(contact=>{
-        return contact.id===recipient
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations(prevConversations=>{
+      let madeChange=false;
+      const newMessage={sender,text}
+      const newConversations=prevConversations.map(conversation=>{
+        if(arrayEquality(conversation.recipients,recipients)){
+          madeChange=true;
+          return{
+            ...conversation,
+            messages:[...conversation.messages,newMessage]
+          }
+        }
+        return conversation;
       })
-      const name=(contact && contact.name )|| recipient;
-      return {id:recipient,name}
+      if(madeChange){
+        return newConversations;
+      }else{
+        return[
+          ...prevConversations,{recipients,messages:[newMessage]}
+        ]
+      }
     })
-    const selected=index===selectedConversationIndex
-    return {...conversations,recipients,selected}
-  })
-  const value={
-    conversations:formattedConversations,
-    selectConversationIndex:setSelectedConversationIndex,
-    createConversations
   }
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id });
+  }
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+      const name = (contact && contact.name) || recipient;
+      return { id: recipient, name };
+    });
+    const selected = index === selectedConversationIndex;
+    return { ...conversations, recipients, selected };
+  });
+  const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    selectConversationIndex: setSelectedConversationIndex,
+    sendMessage,
+    createConversations,
+  };
   return (
     <ConversationsContext.Provider value={value}>
       {children}
     </ConversationsContext.Provider>
   );
+}
+
+
+function arrayEquality(a,b){
+  if(a.length !== b.length) return false;
+  a.sort();
+  b.sort();
+  return a.every((element,index)=>{
+    return element===b[index]
+  })
 }
